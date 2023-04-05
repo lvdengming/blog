4d154f2e8935c949f132af296449df647e1c14c0
lvdengming <lvdengming@foxmail.com>
2023-04-05 13:43:56 +0800

    feat(app): 添加 pwa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,7 +13,42 @@ module.exports = {
         content: 'width=device-width,initial-scale=1,user-scalable=no',
       },
     ],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    [
+      'meta',
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
+    ],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/portrait128.png' }],
+    [
+      'link',
+      { rel: 'mask-icon', href: '/icons/portrait128.svg', color: '#3eaf7c' },
+    ],
+    [
+      'meta',
+      { name: 'msapplication-TileImage', content: '/icons/portrait128.png' },
+    ],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
+    ['meta', { name: 'baidu-site-verification', content: 'code-VjoGmCBp2v' }],
+    ['meta', { name: 'description', content: '吕登名的技术博客~' }],
+    [
+      'meta',
+      {
+        name: 'keywords',
+        content:
+          '吕登名,lvdengming,博客,前端,后端,算法,JS,JavaScript,开发,编程,编程知识,CSS,Nginx,Docker,Shell,Nest,Flutter',
+      },
+    ],
   ],
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+    },
+  },
+  markdown: {
+    externalLinks: { target: '_blank', rel: 'nofollow noopener noreferrer' },
+  },
   port: 8080,
   theme: 'reco',
   themeConfig: {
@@ -63,6 +98,11 @@ module.exports = {
     startYear: '2022',
   },
   plugins: [
+    '@vuepress/pwa',
+    {
+      serviceWorker: true,
+      updatePopup: true,
+    },
     '@vuepress/plugin-medium-zoom',
     '@vuepress-reco/back-to-top',
     '@vuepress-reco/loading-page',
