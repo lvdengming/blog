232dcdc48eb8f34a902cbf01ab10c4e1be1c3e03
lvdengming <lvdengming@foxmail.com>
2024-05-22 20:43:29 +0800

    refactor: 更新eslint部分规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,24 @@
 // 配置参考：https://zh-hans.eslint.org/docs/latest/use/getting-started
 
+const RULE_TYPE = {
+  OFF: 'off',
+  WRAN: 'warn',
+  ERROR: 'error'
+};
+
 module.exports = {
-    env: {
-        browser: true,
-        commonjs: true,
-        es2021: true
-    },
-    extends: 'airbnb-base',
-    overrides: [],
-    parserOptions: {
-        ecmaVersion: 'latest'
-    },
-    rules: {}
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: 'airbnb-base',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    indent: [RULE_TYPE.ERROR, 4],
+    'comma-dangle': [RULE_TYPE.ERROR, 'never']
+  }
 };
